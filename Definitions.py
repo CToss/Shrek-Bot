5a61a4a2732504abc6dfe92b55be1b64e58e7cfa
CToss <mielcarek.brandon@gmail.com>
2021-11-14 21:56:30 -0800

    Shrek bot v 1.0
    Initial commit of shrek bot
    
    Changes voice channel general 2 to swamp at pdt 20:00 and 6:00

---
+++ Definitions.py
@@ -0,0 +1,42 @@
+import pytz
+import datetime
+
+# Timezone defines
+
+
+def PDT():
+    tz = pytz.timezone('America/Los_Angeles')
+    pdtime = datetime.datetime.now(tz)
+    pdtout = pdtime.strftime("%H:%M")
+    return(pdtout)
+
+
+def UTC():
+    dt = datetime.time(16, 0, 0, tzinfo=pytz.UTC)
+    dtp = dt.strftime("%H:%M:%S")
+    return(dtp)
+
+
+def checktime():
+    t = datetime.datetime.utcnow()
+    current_time = datetime.datetime.time(t).strftime("%H:%M:%S")
+    return(current_time)
+
+# channel defines
+
+
+def gen1_channel(client):
+    return(client.get_channel(841047848295333918))
+
+
+def gen2_channel(client):
+    return(client.get_channel(870877538543616060))
+
+
+def botcom_channel(client):
+    return(client.get_channel(841047847713112081))
+
+# def swamptime():
+
+
+# Channel name swap = General 2 into Swamp
---
+++ main.py
@@ -0,0 +1,64 @@
+# Discord package install
+import asyncio
+from asyncio.tasks import wait
+import discord
+from discord.channel import VoiceChannel
+import Definitions
+import os
+import dotenv
+from discord.ext import tasks
+
+
+# env request
+
+dotenv.load_dotenv()
+
+# Client setup
+
+client = discord.Client()
+
+# var defines
+
+
+# Client async function
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    time_check.start()
+
+# User input time check
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if '!time' in message.content.lower():
+        await message.channel.send(Definitions.PDT())
+
+# Check time async
+
+
+@tasks.loop(minutes=10)
+async def time_check():
+    gen2 = Definitions.gen2.channel(client)
+    pdt = Definitions.PDT()
+    print('I am looping')
+    # Check PDT Time
+    if (pdt >= ('20:00') or pdt <= ('6:00')) and gen2.name == '「🔊」General│#2':
+        await gen2.edit(name='「👿」Swamp')
+        print('Changed to Swamp')
+        print(pdt)
+    elif gen2.name == 'Swamp':
+        await gen2.edit(name='Rejects')
+        print('Changed to Rejects')
+        print(pdt)
+
+
+@time_check.before_loop
+async def before_task():
+    await client.wait_until_ready()
+
+client.run(os.getenv(key='TOKEN'))
