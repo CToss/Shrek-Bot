7b4cae3f62a14862a4ed92aaf08efe9391d58e8b
CToss <mielcarek.brandon@gmail.com>
2021-11-15 22:49:58 -0800

    i'm so dumb
    s;dlfja;dslfj

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ async def time_check():
     pdt = Definitions.PDT()
     #print('I am looping')
     # Check PDT Time
-    if (pdt >= (2000) or pdt <= (600)) and gen2.name == '「🔊」General│#2':
+    if (pdt >= (2000) and pdt <= (600)) and gen2.name == '「🔊」General│#2':
         await gen2.edit(name='「👿」Swamp')
         print('Changed to Swamp')
         print(pdt)
-    else:
+    elif (pdt > 600 and pdt < 2000) and gen2.name == '「👿」Swamp':
         await gen2.edit(name='「🔊」General│#2')
         print('Changed to Gen2')
         print(pdt)
